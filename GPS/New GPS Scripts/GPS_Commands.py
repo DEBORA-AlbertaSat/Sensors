14b084e80a10fc67971d911fc26535db3d34e51b
TeddyTubby <122578441+TeddyTubby@users.noreply.github.com>
2023-03-06 15:36:14 -0700

    Added NMEA parsing, added authors

--- GPS/New GPS Scripts/GPS_Commands.py
+++ GPS/New GPS Scripts/GPS_Commands.py
@@ -1,14 +1,17 @@
-# This python script is used to configure and send commands to the Copernicus II GPS Module
+##########################################################################################
+# 
+#This python script is used to configure and send commands to the Copernicus II GPS Module
 #
 # Refer to the documntation for NMEA message format for commands
 #
 # The input and output of each message is in NMEA 0183 format (you can read more in the last
 # chapter of the Copernicus II documentation)
 #
-# ** Important note: Each command must be accompanied with a two digit hex checksum. It's not that
-# intuitive to understand how to find this but basically you need to convert each character of a 
-# command/message '$' and '*' from ascii into hex. Then, consecutively XOR the lower half bytes (nibble).
-# Do the same for the upper half byte. The result of both these operations is the checksum.
+# ** Important note: Each command must be accompanied with a two digit hex checksum. It's 
+# not that easy to understand how to find this but basically you need to convert each  
+# character of a command/message '$' and '*' from ascii into hex. Then, consecutively XOR 
+# the lower half bytes (nibble).Do the same for the upper half byte. The result of both 
+# these operations is the checksum.
 #
 # Example 1: "$PTNLSKG,,,*" => 0x24 0x50 0x54 0x4E 0x4C 0x53 0x4B 0x47 0x2C 0x2C 0x2C 0x2A 
 #	Lower Half Byte XOR = 4 XOR 0 XOR 4 XOR E XOR C XOR ... = 5
@@ -17,7 +20,11 @@
 #
 #	So the command here would be $PTNLSKG,,,*75
 #
-
+##########################################################################################
+#
+# Written by Ryan Bererton and Destin Chan
+#
+##########################################################################################
 import time
 import serial
 
@@ -26,7 +33,7 @@ command = "$PTNLQBA*54"
 
 # Here are some common commands (with their checksums calculated):
 #
-# Check Antenna Status (connected/disconnected) - REQUIRES a ANTENNA DETECTION CIRCUIT TO WORK: $PTNLQBA*54
+# Check Antenna Status (connected/disconnected): $PTNLQBA*54
 #
 # Set GPS Receiver to AIR mode: $PTNLSCR,,,,,,,3,,*5B
 # Set GPS Receiver to LAND mode: $PTNLSCR,,,,,,,1,,*59
@@ -56,7 +63,7 @@ txData = command + "\r\n"
 ser.write(txData.encode())
 print ("\033[1mTransmitted Command: " + command + "\033[0m")
 
-# Feeds a couple of Rx transmissions mainly to see the response/acknowledgement of the command from the GPS receiver
+# Outputs the response or acknowledgement from the GPS receiver
 for count in range(10):
 	Data = ser.readline().decode()
 	print (Data)
--- GPS/New GPS Scripts/GPS_Script.py
+++ GPS/New GPS Scripts/GPS_Script.py
@@ -1,12 +1,21 @@
-# This python script is used to Read GPS data from the GPS Module Copernicus II
+##########################################################################################
 #
+# This python script is used to Read and parse GPS data from the GPS Module Copernicus II 
+# It saves two files. The first file saves the raw NMEA 0183 sentences, while the 
+# other file saves the DD latitude and longitude parsed from the NMEA 0183 sentences
+#
+##########################################################################################
+#
+# Written by Ryan Bererton and Destin Chan
+#
+##########################################################################################
 
 import time
 import serial
 import datetime
 
 Date = str(datetime.date.today())
-nemaFile = "NEMA_Data_" + Date + ".txt"
+nmeaFile = "NMEA_Data_" + Date + ".txt"
 gpsFile = "GPS_Data_" + Date + ".txt"
 
 ser = serial.Serial(
@@ -18,16 +27,54 @@ ser = serial.Serial(
         timeout = 1
         )
 
+# Function to parse NMEA DDM to DD
+def nmeaParse(gpgga):
+	# Get DD format
+	deg = round(float(gpgga[:-8]), 6)
+	minute = round(float(gpgga[-8:]) / 60, 6)
+	return str(deg + minute)
+
 while 1:
-	Data = ser.readline().decode()
-	locTimestamp = time.strftime("%H:%M:%S,UTC%z", time.localtime()) # Get's local time + UTC offset
-	nemaData = "[" + locTimestamp + "] " + Data
-	print(nemaData)
-	
-	# Writes Raw NEMA transmissions to a file
-	with open(nemaFile, 'a', encoding = 'utf-8') as NEMA_File:    
-		NEMA_File.writelines(nemaData)
-	
-	# Writes Parsed GPS data to a separate file
-	# with open(gpsFile, 'a', encoding = 'utf-8') as GPS_File:    
-		# GPS_File.writelines(gpsData)	
+	opData = ser.readline().decode()
+	locTimestamp = "[" + time.strftime("%H:%M:%S,UTC%z", time.localtime()) + "] " # Get's local time + UTC offset
+	print(locTimestamp + opData)
+		
+	# Checks for valid NMEA 0183 sentence:
+	if opData[0] == "$":
+		# Writes Raw NMEA transmissions to a file
+		with open(nmeaFile, 'a', encoding = 'utf-8') as NMEA_File:    
+			NMEA_File.writelines(locTimestamp + opData)
+
+		# Splitting NMEA Sentence
+		validGPS = opData.split(",")
+		
+		# Check if valid GPS fix + # of satellites
+		fixQuality = validGPS[6]
+		if fixQuality == "1":
+			fixState = "GPS Fix: Valid"
+		elif fixQuality == "2":
+			fixState = "GPS Fix: Valid (SBAS)"
+		else:
+			fixState = "GPS Fix: Invalid"
+
+		sat = "# of Sats: " + validGPS[7].lstrip("0") + ", "
+
+		# Extracting DDM latitude, longitude and altitude
+		if validGPS[0] == "$GPGGA" and len(validGPS[2]) != 0:
+			lat = nmeaParse(validGPS[2])
+			lon = nmeaParse(validGPS[4])
+			
+			loc = "Lat/Lon: (" + lat + "," + lon + "), "
+
+			if len(validGPS[9]) != 0:
+				alt = "Alt: " + validGPS[9].lstrip("0") + " m, "
+			else:
+				alt = "Alt: XXX m, "
+
+			# Writes Parsed GPS data to a separate file
+			with open(gpsFile, 'a', encoding = 'utf-8') as GPS_File:    
+				GPS_File.writelines(locTimestamp + loc + alt + sat + fixState + "\n")
+
+		elif validGPS[0] == "$GPGGA" and len(validGPS[2]) == 0:
+			with open(gpsFile, 'a', encoding = 'utf-8') as GPS_File:
+				GPS_File.writelines(locTimestamp + "Lat/Lon: (XXX,XXX), Alt: XXX m, " + sat + fixState + "\n")
