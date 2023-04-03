8645a24a3ce447e276aa820bbf88a6f08b891bd0
Devin Headrick <dheadrick1618@gmail.com>
2023-04-03 14:37:37 -0600

    tided up the bme code. Tested on pi. It is working but needs to be finished still

--- Humidity/bme280/bme280_collect.py
+++ Humidity/bme280/bme280_collect.py
@@ -6,25 +6,18 @@ import sys
 
 import csv
 
+import array
+
 from datetime import datetime, timezone
 
-header = ["SysTime, Humidity, Pressure, Altitude, Temperature"]
+header = ["SysTime, Humidity, Pressure, Altitude [m], Temperature [C]"]
 
-data_out_file_name = "~/DEBORA/Sensors/Data/bme280_data"
+data_out_file_name = "/home/albertasat/DEBORA/Sensors/Data/bme280_data/test_data.csv"
 
 def write_to_file(data_line):
-	"""
-    Desc: 
-        This file appends a line of data to the output file (writes). This fxn assumes the file has already been open
-    Args: 
-        data_out_file : This is a reference to the file we are writing data to
-        data_line : String array holding the data we are writing to the CSV file 
-	Return:
-        Nothing
-    """
 
     # open the file
-    f = open(data_out_file_name, 'w')
+    f = open(data_out_file_name, 'a')
 
     # init the writer
     writer = csv.writer(f)
@@ -32,13 +25,16 @@ def write_to_file(data_line):
     # write the data 
     writer.writerow(data_line)
 
+
+
     # close the file 
     f.close()
-
-	return 
+    
+    return 
 
 
 def init_bme280():
+    
     print("\nSparkFun BME280 Sensor Init... \n")
     bme = qwiic_bme280.QwiicBme280()
 
@@ -53,31 +49,28 @@ def init_bme280():
 
 
 def query_bme280(bme):
- """
-    Desc:
-        Query
-    """
+
     #TODO: Check if data is valid. (i.e. length, data values, etc). If invalid return -1
 
     res = []
 
     humidity = bme.humidity
     pressure = bme.pressure
-    altitude_feet = bme.altitude_feet
-    temperature = bme.temperature_fahrenheit
+    altitude = bme.altitude_meters
+    temperature = bme.temperature_celsius
 
-    print("Humidity:\t%.3f" % bme.humidity)
-    print("Pressure:\t%.3f" % bme.pressure)    
-    print("Altitude:\t%.3f" % bme.altitude_feet)
-    print("Temperature:\t%.2f" % bme.temperature_fahrenheit)       
+    print("Humidity:\t%.3f" % humidity)
+    print("Pressure:\t%.3f" % pressure)    
+    print("Altitude:\t%.3f" % altitude)
+    print("Temperature:\t%.2f" % temperature)       
     print("\n")
 
     res.append(humidity)
     res.append(pressure)
-    res.append(altitude_feet)
+    res.append(altitude)
     res.append(temperature)
 
-    return 
+    return res
 
 
 
@@ -88,23 +81,30 @@ if __name__ == '__main__':
     write_to_file(header)
     
     #initialize the ICM device
-    bme_obj = init_icm20948()
+    bme_obj = init_bme280()
 
     #Begin inf loop writing data to file.
     while True:
+
         # fetch icm data. 
         #If -1 returned, data was not fetched and ignore this loop
         bme_data = query_bme280(bme_obj);
 
-        if bme_data == -1
+        print(bme_data)
+
+        if bme_data == -1:
             break
-        else    
+        else:
+            
             #fetch system time
-            sys_time = datetime.utcnow().strftime("%Y%m%d%I:%M%p")
+            sys_time = datetime.utcnow().strftime("%Y%m%dT%H:%M:%S.%f")
+
+
+            # sys_time = "TESTS"
 
             #create string array containing sys time and icm data
             data_row = [sys_time]
-            data_row = array.array(sys_time, bme_data)
+            data_row.extend(bme_data)
 
             #write data row to csv file
             write_to_file(data_row)
